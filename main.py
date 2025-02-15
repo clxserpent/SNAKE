f56ef60098f00bc5a31a1ea888fb960d6c1e0340
Serpent <itzgalaxy1235@gmail.com>
2025-02-15 19:23:20 +0000

    final commit

--- main.py
+++ main.py
@@ -4,4 +4,7 @@ HEIGHT = 800
 WIDTH = 800
 display = pygame.display.set_mode((HEIGHT,WIDTH))
 game = Game(HEIGHT,WIDTH,display)
+icon = pygame.image.load("snake.ico")
+pygame.display.set_icon(icon)
+pygame.display.set_caption('Snake')
 game.run()
